dcd4323fed4c88e627cb53272edd068a80d49217
rodriknd <rodrigopacehco965@gmail.com>
2025-02-16 18:26:31 -0400

    Agregando el diseño de la vista principal de la landing

--- frontend/src/app/modules/landing/home/home.component.ts
+++ frontend/src/app/modules/landing/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,13 +9,93 @@ import { RouterLink } from '@angular/router';
     templateUrl  : './home.component.html',
     encapsulation: ViewEncapsulation.None,
     standalone   : true,
-    imports      : [MatButtonModule, RouterLink, MatIconModule],
+    imports      : [MatButtonModule, RouterLink, MatIconModule, CommonModule],
 })
 export class LandingHomeComponent
 {
-    /**
-     * Constructor
-     */
+    persons_list = [
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',            
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+        {
+            name: 'Nataniel L. Heardy',
+            age: 25,
+            height: 1.80,
+            weight: 80,
+            hair: 'Liso',
+            emergency_contact: '+591 12345678',
+            last_location: 'Calle 123 Nº 1 Provincia Buenos Aires de la ciudad de Cochabamba',
+        },
+    ];
     constructor()
     {
     }
